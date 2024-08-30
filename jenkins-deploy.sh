8767f0af270b37355fe8db787cfed562c6881f1e
OlivierKouokam <62020582+OlivierKouokam@users.noreply.github.com>
2024-08-30 18:18:20 +0100

    Update jenkins-deploy.sh

--- jenkins-deploy.sh
+++ jenkins-deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Make sure jenkins data folder exit
-sudo mkdir -p $HOME/data/jenkins-pv
+mkdir -p $HOME/data/jenkins-pv
 
 # Get the home directory
 HOME_PATH=$HOME
 
+# Make jenkins-deployment.yml writtable
+sudo chmod 777 jenkins-deployment.yml
+
 # Replace the placeholder in the template YAML with the actual path
-sudo sed "s|{{HOME_PATH}}|$HOME_PATH|g" jenkins-deployment.yml | sudo kubectl apply -f -
+sed "s|{{HOME_PATH}}|$HOME_PATH|g" jenkins-deployment.yml | sudo kubectl apply -f -
